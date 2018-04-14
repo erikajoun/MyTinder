c77965f4837637fe1493e9b3ce7a0202b9f26bdd
ejoun6718 <hjoun@ucsd.edu>
2018-04-13 22:15:37 -0700

    Return image to original position after drag

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class CardsViewController: UIViewController {
   
   var cardInitialCenter: CGPoint!
+  var cardInitialRotation: CGAffineTransform!
+  var rotationSpeed = 1.0
 
   @IBAction func didPanImage(_ sender: UIPanGestureRecognizer) {
     let imageView = sender.view as! UIImageView
     let translation = sender.translation(in: view)
     let velocity = sender.velocity(in: view)
     let location = sender.location(in: view)
+    let imageY = view.frame.origin.y
+    let imageHeight = view.frame.height
+    let imageHalf = 210
     
     //print("translation \(translation)")
     //print("velocity \(velocity)")
@@ -24,21 +29,21 @@ class CardsViewController: UIViewController {
     
     if sender.state == .began {
       cardInitialCenter = imageView.center
+      cardInitialRotation = imageView.transform
       print("began")
       
-    } else if sender.state == .changed {
-      /*imageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y + translation.y)*/
-      print("changed")
-      
-      let imageY = view.frame.origin.y
-      let imageHeight = view.frame.height
-      let imageHalf = 210
-      
-      var rotationSpeed = 1.0
       if(location.y >= CGFloat(imageHalf)) {
         rotationSpeed = -1
       }
+      else {
+        rotationSpeed = 1
+      }
       
+    } else if sender.state == .changed {
+      imageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y)
+      print("changed")
+      
+      // Rotation
       if(velocity.x > 0) {
         imageView.transform = imageView.transform.rotated(by: CGFloat(rotationSpeed * Double.pi / 180))
       }
@@ -46,7 +51,11 @@ class CardsViewController: UIViewController {
         imageView.transform = imageView.transform.rotated(by: CGFloat(-rotationSpeed * Double.pi / 180))
       }
     } else if sender.state == .ended {
-      
+      UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                     animations: { () -> Void in
+                      imageView.center = self.cardInitialCenter
+                      imageView.transform = self.cardInitialRotation
+      }, completion: nil)
     }
   }
   
