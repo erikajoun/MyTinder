2634a96db83cc5c41fb3982126d4e77f1070e22b
ejoun6718 <hjoun@ucsd.edu>
2018-04-13 22:37:21 -0700

    Add profile view

---
+++ Tinder/ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  Tinder
+//
+//  Created by Hye Lim Joun on 4/13/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+  @IBOutlet weak var profileImageView: UIImageView!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //profileImageView.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
